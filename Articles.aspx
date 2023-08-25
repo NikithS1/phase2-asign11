f45f67b94700522d1fc0e3ea99414e64db762d69
Nikiths1 <nikith.s1@mphasis.com>
2023-08-25 15:41:13 +0530

    first commit

---
+++ Articles.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="Articles.aspx.cs" Inherits="YourNamespace.Articles" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Articles</h2>
+    <asp:Repeater ID="articleRepeater" runat="server">
+        <ItemTemplate>
+            <div>
+                <h3><%# Eval("Title") %></h3>
+                <p><%# Eval("Content") %></p>
+                <p>Publish Date: <%# Eval("PublishDate") %></p>
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
+</asp:Content>
+
+
+
+
+
---
+++ SiteMaster.Master.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace YourNamespace
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load articles on initial page load
+                LoadArticles();
+            }
+        }
+
+        private void LoadArticles()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionString"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT ArticleId, Title, Content, PublishDate FROM Articles";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Use the reader to populate your UI elements or store data
+                // For example, bind data to a GridView or Repeater control
+                // gridView.DataSource = reader;
+                // gridView.DataBind();
+
+                reader.Close();
+            }
+        }
+    }
+}
